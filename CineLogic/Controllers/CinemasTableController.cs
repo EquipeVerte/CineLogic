67289f1365225d7875fa8126fee20658c2287d9d
Mylene Marois <mylenemarois@hotmail.com>
2020-05-12 14:53:12 -0400

    Test d'une table

---
+++ CineLogic/Controllers/CinemasTableController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CineLogic.Models;
+
+namespace CineLogic.Controllers
+{
+    public class ShowDataController : Controller
+    {
+        private CineDBEntities db = new CineDBEntities();
+
+        // GET: /ShowData/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        ModelServices mobjModel = new ModelServices();
+
+        public ActionResult CinemaTable()
+        {
+            var cinemas = db.Cinemas;
+            return View(cinemas.ToList());
+        }
+
+        [HttpGet]
+        public JsonResult UpdateRecord(int id, string nom, string adresse, bool enExploitation, int responsableID, string programmateur)
+        {
+            bool result = false;
+            try
+            {
+                result = mobjModel.UpdateCinema(id, nom, adresse, enExploitation, responsableID, programmateur);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult SaveRecord(string nom, string adresse, bool enExploitation, int responsableID, string programmateur)
+        {
+            bool result = false;
+            try
+            {
+                result = mobjModel.SaveCinema(nom, adresse, enExploitation, responsableID, programmateur);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult DeleteRecord(int id)
+        {
+            bool result = false;
+            try
+            {
+                result = mobjModel.DeleteCinema(id);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+
+}
---
+++ CineLogic/Models/Cinemas/ICinemaRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CineLogic.Models.Cinemas
+{
+    public interface ICinemaRepository
+    {
+        void CreateCinema(Cinema cinema);
+
+        void DeleteCinema(int id);
+
+        Cinema GetCinema(int id);
+
+        IEnumerable<Cinema> GetAllCinemas();
+
+        IEnumerable<Cinema> GetSallesByCinema(int CinemaID);
+
+        void UpdateCinema(Cinema cinema);
+
+        int SaveChanges();
+
+        void Dispose();
+    }
+}
