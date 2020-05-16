92fe2dc551d4ef750766cc05bb7253484182b990
Andrew Faulkner <andyfaulkner54@gmail.com>
2020-05-16 14:35:41 -0400

    Corriger erreurs de merge

--- CineLogic/Global.asax.cs
+++ CineLogic/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,7 +12,6 @@ namespace CineLogic
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
