cd3850dfa1e1817b7812980c5b34b6bda61f7bf6
Andrew Faulkner <andyfaulkner54@gmail.com>
2020-05-28 15:10:28 -0400

    CSProj fix and edition of order of contents

--- CineLogic/Content/Programmation/AjaxLoader.js
+++
@@ -1,8 +0,0 @@
-﻿$(document)
-    //  Afficher l'icone "loading" pendant que l'ajax s'execute.
-    .ajaxStart(function () {
-        $(".loading").show();
-    })
-    .ajaxStop(function () {
-        $(".loading").hide();
-    });
